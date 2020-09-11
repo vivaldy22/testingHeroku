9adc9ab5e5a6660ba73063ad247151cf1a10c54f
Vivaldy Andhira <dididlav@gmail.com>
2020-09-11 22:59:32 +0700

    heroku

--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ module github.com/vivaldy22/testingHeroku
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/gorilla/mux v1.8.0
+	github.com/spf13/viper v1.7.1
+)
--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
+	"github.com/vivaldy22/testingHeroku/viper"
+	"log"
+	"net/http"
 )
 
 func main() {
-	g := gin.Default()
+	//g := gin.Default()
+	//
+	//g.GET("/", func(ctx *gin.Context) {
+	//	ctx.Writer.Write([]byte("Hai"))
+	//})
+	//
+	//g.Run()
 
-	g.GET("/", func(ctx *gin.Context) {
-		ctx.Writer.Write([]byte("Hai"))
+	port := viper.ViperGetEnv("PORT", "8080")
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Ga cair cair euy :D"))
+	})
+	r.HandleFunc("/cair", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Masih belum cair gan"))
 	})
 
-	g.Run()
+	log.Println("Starting Server...")
+
+	if err := http.ListenAndServe(":"+port, r); err != nil {
+		log.Fatal(err)
+	}
 }
